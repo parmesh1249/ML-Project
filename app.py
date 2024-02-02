aef73b168806807f92da049b9df007698ddd719d
Parmesh <warparmesh88@gmail.com>
2024-02-02 16:36:28 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=="__main__":
     logging.info("The execution is started")
@@ -14,7 +15,12 @@ if __name__=="__main__":
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+         ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
         
